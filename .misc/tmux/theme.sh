a1a0013ac604e1471a17312796872c823915bf01
Anton Tsviatkou <a.tsviatkou@gmail.com>
2017-09-23 09:39:29 -0700

    Move dark theme tmux config into separate file

--- .misc/tmux/theme.sh
+++ .misc/tmux/theme.sh
@@ -3,38 +3,11 @@ set -g status-left-length 32
 set -g status-right-length 150
 set -g status-interval 5
 
-# statusbar colors
-set-option -g status-bg default
-set-option -g status-fg colour136 #faint yellow
-set-option -g status-attr default
+# default to a dark color scheme
+source-file $HOME/.misc/tmux/theme-dark.conf
 
-# window title colors
-set-window-option -g window-status-fg colour242 #faint gray
-set-window-option -g window-status-bg default
-set-window-option -g window-status-attr dim
-
-# active window title colors
-set-window-option -g window-status-current-fg colour249 #bright gray
-set-window-option -g window-status-current-bg default
-set-window-option -g window-status-current-attr bright
-
-# pane border
-set-option -g pane-border-fg colour235 #almost black
-set-option -g pane-active-border-fg colour230 #bright yellow
-
-# message text
-set-option -g message-bg colour235 #almost black
-set-option -g message-fg colour231 #almoust white
-
-# pane number display
-set-option -g display-panes-active-colour colour33 #blue
-set-option -g display-panes-colour colour166 #orange
-
-# clock
-set-window-option -g clock-mode-colour colour64 #green
-
-# bell
-set-window-option -g window-status-bell-style fg=colour235,bg=colour160 #black, red
+if-shell '[ -x "$HOME/bin/color-mode-for-term" ] && [ "$($HOME/bin/color-mode-for-term)" = "light" ]' \
+    'source-file $HOME/.misc/tmux/theme-light.conf'
 
 set -g window-status-format "#I #W"
 set-window-option -g  window-status-current-format "#[bold]#I #W"
@@ -47,7 +20,3 @@ tm_date="%R %d %b"
 
 set -g status-left $tm_session_name' '$tm_window_zoom_indicator
 set -g status-right $tm_date' #h'
-
-
-if-shell '[ -x "$HOME/bin/color-mode-for-term" ] && [ "$($HOME/bin/color-mode-for-term)" = "light" ]' \
-    'source-file $HOME/.misc/tmux/theme-light.conf'
